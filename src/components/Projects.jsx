06dd8182df6786cc1bf20bb599bdbd5f773abd7f
Sarv <137513430+sarv-projects@users.noreply.github.com>
2025-09-08 11:59:17 +0530

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -9,11 +9,21 @@ authentication and RBAC.
 •Developed a React frontend with product catalog, search, shopping cart, and checkout flow; styled using Material-UI and Tailwind
 CSS for a modern, user-friendly responsive UI.
 •Designed and managed a relational database (PostgreSQL) for product, user, and order management with transaction integrity.
-•Added payment simulation with Razorpay, reviews, and recommendations to enhance UX.
+•Added payment simulation with Razorpay, reviews to enhance UX.
 `.trim()
   },
   {
-    title: "2. URL Shortener Backend",
+    title: "2. Real-Time Chat Application",
+    tech: "Python, Flask, WebSocket, HTML/CSS/JS",
+    description: `
+•Engineered a WebSocket-based chat system enabling instant (<200ms) message delivery for multiple rooms.
+•Designed lightweight frontend in HTML/CSS/JS seamless real-time interaction.
+•Persisted chat history in SQLite, supporting 7-day retrieval and scaling to 500+ concurrent sessions.
+`.trim()
+  }
+  ,
+  {
+    title: "3. URL Shortener Backend",
     tech: "Java, Spring Boot, PostgreSQL",
     description: `
 •Developed a scalable URL shortening backend generating unique 6-character short codes with fast redirection.
@@ -22,7 +32,7 @@ storage, and implemented robust input validation.
 `.trim()
   },
   {
-    title: "3. Notification Microservice ",
+    title: "4. Notification Microservice ",
     tech: " Java, Spring Boot, REST API, SMTP",
     description: `
 •Built a reusable microservice for sending notifications via email, extendable to SMS/push, serving 100+ daily
@@ -34,15 +44,16 @@ handling.
 `.trim()
   },
   {
-    title: "4. Automated CI/CD Pipeline for a Microservice",
+    title: "5.CI/CD Pipeline for Django App",
     tech: "GitHub Actions, Docker, Ansible, Kubernetes",
     description: `
-•Built a reduced-downtime CI/CD pipeline for a containerized Spring Boot application, reducing deployment time by 70%.
-•Implemented multi-stage Docker builds for optimized image size and configured Ansible playbooks for automated Kubernetes deployments.
+•Automated build, test, and deployment workflow for a Django Task Manager app using GitHub Actions, cutting manual release effort
+by ~60%.
+•Containerized the app with Docker, ensuring consistent builds across environments and reducing deployment errors.
 `.trim()
   },
   {
-    title: "5. Personal Portfolio Website",
+    title: "6. Personal Portfolio Website",
     tech: "React.js, JavaScript, Netlify",
     description: `
 • Designed and deployed a responsive personal portfolio using React.js and hosted it via Netlify.
@@ -69,6 +80,7 @@ function Projects() {
 }
 
 export default Projects;
+
 
 
 
